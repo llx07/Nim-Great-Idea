687cdb5fd24272b1fd2bf2ac4ccc0984556cf213
jbn-0766 <2179375614@qq.com>
2025-10-15 17:20:02 +0800

    feat: fill in NimEnv (#3)
    * feat: fill in NimEnv
    
    * fix nim.py
    
    ---------
    
    Co-authored-by: JBN-0766 <jbn-0766>
    Co-authored-by: Lexiao Lin <2623459403@qq.com>

--- nim.py
+++ nim.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+
 class NimEnv:
     """The environment for Nim.
 
@@ -8,9 +9,6 @@ class NimEnv:
         player: the current player (`0` or `1`)
         winner: the winner of the game, or `None` if the game is till in process
     """
-
-    WIN_REWARD = 100.0
-    ELSE_REWARD = 0.0
 
     def __init__(self, initial: None | list[int] = None) -> None:
         """Initialize the environment.
@@ -24,41 +22,60 @@ class NimEnv:
             self.piles = initial
         self.player = 0
         self.winner = None
-    
+
     def get_available_actions(self) -> list[tuple[int, int]]:
         """Return all available actions according to `self.piles`.
 
         Returns:
             A list of tuple in format (pile_number, count).
         """
-        raise NotImplementedError # TODO
+        actions = []
+        for pile_number, stones in enumerate(self.piles):
+            for count in range(1, stones + 1):
+                actions.append((pile_number, count))
+        return actions
 
-    def move(self, action: tuple[int, int]) -> float:
+    def move(self, action: tuple[int, int]) -> int:
         """Make the move `action`.
 
-        This method will update `self.piles`, `self.player` and `self.winner`.
+        If the action is invalid, this method will return -1 and do nothing.
+        Otherwise, this method will update `self.piles`, `self.player` and 
+        `self.winner`.
 
-        If `self.winner` is not None, calling this method will cause an
-        RuntimeError. If `action` is invalid, calling the method will cause
-        an ValueError.
 
         Args:
             action: the action in format (pile_number, count)
 
         Returns:
-            A float value for reward. return `WIN_REWARD` if the current player
-            wins, return `ELSE_REWARD` otherwise.
-
-        Raises:
-            ValueError: if the action is invalid.
-            RuntimeError: if the game is already end.
+            A int indicate the game status.
+            `-1` for invalid action, `1` for game continue, `0` for game ends.
         """
-        raise NotImplementedError # TODO
+        if self.winner is not None:
+            return -1
+
+        pile_number, count = action
+        if pile_number < 0 or pile_number >= len(self.piles):
+            return -1
+
+        if count < 1 or count > self.piles[pile_number]:
+            return -1
+
+        self.piles[pile_number] -= count
+
+        if all(stones == 0 for stones in self.piles):
+            self.winner = self.player
+            return 0
+
+        else:
+            self.player ^= 1
+            return 1
+
 
 class Agent(ABC):
     """
     Abstract Base Class for AI
     """
+
     @abstractmethod
     def choose_action(self, state: NimEnv) -> tuple[int, int]:
         pass
